662f07cdbbdebc49057aa2611a96e5d8da087b20
abdkarim1229 <abdulkarimklj@gmail.com>
2021-09-08 13:38:41 +0700

    second-commit

--- src/main/kotlin/pembulatan_nilai.kt
+++ src/main/kotlin/pembulatan_nilai.kt
@@ -1,17 +1,18 @@
 fun main() {
     val angka = Angka()
-    angka.angka1 = 0.51
-    println("Pembulatan angkanya adalah : ${angka.angka1.toInt()}")
+    angka.angka1 = 15.51
+    println("~ Pembulatan Angka 15 & 16 ~")
+    println("Angkanya adalah : ${angka.angka1.toInt()}")
 }
 
 class Angka {
     var angka1: Double = 0.0
         get() = field
         set(value) {
-            field = if (value > 0.5)
-                1.0
-            else if (value < 0.5)
-                0.0
+            field = if (value < 15.5)
+                15.0
+            else if (value > 15.5)
+                16.00
             else
                 value
         }
